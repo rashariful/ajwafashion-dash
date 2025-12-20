342fbc247c592d3b755e496daa682293d099f72e
rashariful <ra.mdsharifulislam@gmail.com>
2025-12-21 01:45:32 +0600

    courier button

--- src/pages/Order/OrderPage.tsx
+++ src/pages/Order/OrderPage.tsx
@@ -87,7 +87,8 @@ refetch()
           type="primary"
           size="small"
           onClick={() => sendToCourier(record._id)}
-          disabled={record.status !== "pending"}
+          disabled
+          // disabled={record.status !== "pending"}
         >
           Send to Courier
         </Button>
